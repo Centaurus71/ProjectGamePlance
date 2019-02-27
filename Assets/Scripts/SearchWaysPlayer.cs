e55454a74cda981b0d7e8ce0dde490002c74af7f
Centaurus71 <Ruslan-2045@mail.ru>
2019-02-27 23:59:30 +0300

    Ruslan made changes

---
+++ Assets/Scripts/SearchWaysPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Operation
+{
+    Add = '+',
+    Compute = '-'
+}
+
+public class SearchWaysPlayer : MonoBehaviour
+{
+    public class SearchWays : MonoBehaviour
+    {
+        public void EmptyPlayerCell(List<Vector3> emptySpace, int randomNumber, Vector3 vectorPlay, Vector3 vectorDirection, Operation op)
+        {
+
+        }
+    }
+}
